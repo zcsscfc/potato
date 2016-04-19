551d2cfce1257113bb4f2c60a3b9e7bd06542ebd
lance <lance_li@outlook.com>
2016-04-19 23:39:15 +0800

    供参考，安卓App添加4.4版本的沉浸式状态导航栏效果
    修改 MainActivity.java 修改 activity_main.xml 修改 styles.xml
    新增 SystemBarTintManager.java

--- ui/app/src/main/java/com/android/potato/MainActivity.java
+++ ui/app/src/main/java/com/android/potato/MainActivity.java
@@ -1,7 +1,9 @@
 package com.android.potato;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 创建状态栏的管理实例
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        // 激活状态栏设置
+        tintManager.setStatusBarTintEnabled(true);
+        // 激活导航栏设置
+        tintManager.setNavigationBarTintEnabled(true);
+        // 设置颜色
+        tintManager.setStatusBarTintResource(R.color.colorPrimary);
+        tintManager.setNavigationBarTintResource(R.color.colorPrimary);
+        // 设置边距，保证对齐
+        SystemBarTintManager.SystemBarConfig config = tintManager.getConfig();
+        TextView textViewHello = (TextView)findViewById(R.id.textViewHello);
+        textViewHello.setPadding(0, config.getPixelInsetTop(true), 0, config.getPixelInsetBottom());
     }
 }
