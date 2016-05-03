1c3b6503fe627acb2b36f6b2f15a01a4e67b2787
TOP601 <top601studio@gmail.com>
2016-05-03 16:52:33 +0800

    添加左侧导航菜单

---
+++ ui/app/src/main/java/com/android/potato/LeftMenuListAdapter.java
@@ -0,0 +1,72 @@
+package com.android.potato;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by s1112001 on 2016/5/3.
+ */
+public class LeftMenuListAdapter extends BaseAdapter {
+
+    private List<Map<String, Object>> data;
+    private LayoutInflater layoutInflater;
+    private Context context;
+    public LeftMenuListAdapter(Context context,List<Map<String, Object>> data){
+        this.context=context;
+        this.data=data;
+        this.layoutInflater=LayoutInflater.from(context);
+    }
+    /**
+     * 组件集合，对应leftmenu_item.xml中的控件
+     */
+    public final class leftMenu{
+        public ImageView iv_icon;
+        public TextView tv_name;
+    }
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+    /**
+     * 获得某一位置的数据
+     */
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+    /**
+     * 获得唯一标识
+     */
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        leftMenu LeftMenu=null;
+        if(convertView==null){
+            LeftMenu=new leftMenu();
+            //获得组件，实例化组件
+            convertView=layoutInflater.inflate(R.layout.leftmenu_item, null);
+            LeftMenu.iv_icon=(ImageView)convertView.findViewById(R.id.iv_icon);
+            LeftMenu.tv_name=(TextView)convertView.findViewById(R.id.tv_name);
+            convertView.setTag(LeftMenu);
+        }else{
+            LeftMenu=(leftMenu)convertView.getTag();
+        }
+        //绑定数据
+        LeftMenu.iv_icon.setBackgroundResource((Integer)data.get(position).get("iv_icon"));
+        LeftMenu.tv_name.setText((String)data.get(position).get("tv_name"));
+        return convertView;
+    }
+
+}
--- ui/app/src/main/java/com/android/potato/MainActivity.java
+++ ui/app/src/main/java/com/android/potato/MainActivity.java
@@ -2,10 +2,26 @@ package com.android.potato;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends FragmentActivity {
+    private DrawerLayout mDrawerLayout = null;
+    private ListView listView = null;
+    LeftMenuListAdapter leftMenuListAdapter = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +38,41 @@ public class MainActivity extends FragmentActivity {
         // 设置边距，保证对齐
         SystemBarTintManager.SystemBarConfig config = tintManager.getConfig();
         LinearLayout linearLayoutTitleContainer = (LinearLayout) findViewById(R.id.linearLayoutTitleContainer);
-        linearLayoutTitleContainer.setPadding(0, config.getPixelInsetTop(true)/3+10, 0, 0);
+        linearLayoutTitleContainer.setPadding(0, config.getPixelInsetTop(true) / 3 + 10, 0, 0);
+
+        //左侧导航菜单
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView = (ListView) findViewById(R.id.LeftMenuListView);
+        List<Map<String, Object>> list = getData();
+        leftMenuListAdapter = new LeftMenuListAdapter(this, list);
+        listView.setAdapter(leftMenuListAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                Map<String, String> map = (Map<String, String>) leftMenuListAdapter.getItem(arg2);
+                Toast.makeText(MainActivity.this, map.get("tv_name"), Toast.LENGTH_LONG).show();
+            }
+        });
+        ImageView leftMenu = (ImageView) findViewById(R.id.leftMenu);
+        leftMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 按钮按下，将抽屉打开
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+    }
+
+    //获取左侧菜单列表数据
+    public List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < 5; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("iv_icon", R.mipmap.ic_launcher);
+            map.put("tv_name", "设置" + i);
+            list.add(map);
+        }
+        return list;
     }
 }
