1b6ddcf816e93b2cf64fbc04686ba47e6d4086b7
zhangcs <zhangcs@Ctrip.com>
2016-05-09 20:19:14 +0800

    udpate api

--- api/src/main/java/com/potato/api/controller/PostController.java
+++ api/src/main/java/com/potato/api/controller/PostController.java
@@ -1,13 +1,13 @@
 package com.potato.api.controller;
 
+import com.potato.api.entity.PostD;
 import com.potato.api.entity.PostM;
 import com.potato.api.framework.bean.Response;
 import com.potato.api.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
 import java.util.List;
 
 /**
@@ -19,9 +19,22 @@ public class PostController {
     @Autowired
     private PostService postService;
 
-    @RequestMapping(value = "post", method = RequestMethod.GET)
-    public Response post() {
-        List<PostM> postMs=postService.getPostList();
+    @RequestMapping(value = "postm", method = RequestMethod.GET)
+    public Response postm() {
+        List<PostM> postMs=postService.getPostMList();
         return new Response().success(postMs);
     }
+
+    @RequestMapping(value = "postm/{postId}",method = RequestMethod.GET)
+    public Response postm(@PathVariable BigInteger postId){
+        PostM postM=postService.getPostMByPostId(postId);
+        return new Response().success(postM);
+    }
+
+    @RequestMapping(value = "postd/{postId}",method = RequestMethod.GET)
+    public Response postd(@PathVariable BigInteger postId){
+        PostD postD=postService.getPostDByPostId(postId);
+        return new Response().success(postD);
+    }
+
 }
--- api/src/main/java/com/potato/api/service/PostService.java
+++ api/src/main/java/com/potato/api/service/PostService.java
@@ -1,10 +1,12 @@
 package com.potato.api.service;
 
+import com.potato.api.entity.PostD;
 import com.potato.api.entity.PostM;
 import com.potato.api.framework.jdbc.dao.DataAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.List;
 
 /**
@@ -16,7 +18,16 @@ public class PostService {
     @Autowired
     private DataAccessor dataAccessor;
 
-    public List<PostM> getPostList(){
+    public List<PostM> getPostMList() {
         return dataAccessor.selectList("selectPostM");
+    }
+
+
+    public PostM getPostMByPostId(BigInteger postId) {
+        return dataAccessor.selectOne("selectPostMPostId", postId);
+    }
+
+    public PostD getPostDByPostId(BigInteger postId) {
+        return dataAccessor.selectOne("selectPostDPostId",postId);
     }
 }
