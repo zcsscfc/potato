c97618454ed8e5d84daf9a4e668955881f805821
TOP601 <top601studio@gmail.com>
2016-05-10 18:15:25 +0800

    content页面获取api数据

---
+++ ui/app/src/main/java/com/potato/model/PostData.java
@@ -0,0 +1,16 @@
+package com.potato.model;
+
+/**
+ * Created by s1112001 on 2016/5/10.
+ */
+public class PostData {
+    private PostDetail data;
+
+    public void setData(PostDetail data) {
+        this.data = data;
+    }
+
+    public PostDetail getData() {
+        return this.data;
+    }
+}
