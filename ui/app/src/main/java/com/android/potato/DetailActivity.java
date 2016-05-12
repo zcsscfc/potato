22ddc910cbd620d64c90c2c6fdc4c54461a1aca0
TOP601 <top601studio@gmail.com>
2016-05-12 15:33:56 +0800

    Detail页面标题栏设置

--- ui/app/src/main/java/com/android/potato/DetailActivity.java
+++ ui/app/src/main/java/com/android/potato/DetailActivity.java
@@ -1,13 +1,22 @@
 package com.android.potato;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,35 +38,104 @@ import okhttp3.Response;
  * Created by s1112001 on 2016/5/10.
  */
 public class DetailActivity extends Activity {
-    private TextView tv_title, tv_source_date;
     private WebView wv_content;
     private android.os.Handler msgHandler;
     private final int msg_get_post_m_list = 0;
     private PostItem postItem;
+    private ImageButton img_btn_back, img_btn_menu;
+    private Button btn_to_read, btn_fav;
+    PopupMenu popupMenu;
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setStatusBarTintResource(R.color.color_11);//通知栏所需颜色
+        }
         setContentView(R.layout.activity_detail);
 
-        postItem = (PostItem) getIntent().getSerializableExtra("postItem");
+        img_btn_back = (ImageButton) findViewById(R.id.img_btn_back);
+        btn_to_read = (Button) findViewById(R.id.btn_to_read);
+        btn_fav = (Button) findViewById(R.id.btn_fav);
+        img_btn_menu = (ImageButton) findViewById(R.id.img_btn_menu);
 
-        ImageButton img_btn_cancel = (ImageButton) findViewById(R.id.img_btn_cancel);
-        img_btn_cancel.setOnClickListener(new View.OnClickListener() {
+        popupMenu = new PopupMenu(this, findViewById(R.id.img_btn_menu));
+        menu = popupMenu.getMenu();
+        // 通过XML文件添加菜单项
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        // 监听事件
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item1:
+                        Toast.makeText(DetailActivity.this, "You Click 菜单1", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.item2:
+                        Toast.makeText(DetailActivity.this, "You Click 菜单2", Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+        img_btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DetailActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+        btn_to_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this, "You Click 待读", Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this, "You Click 收藏", Toast.LENGTH_LONG).show();
+            }
+        });
+        img_btn_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupmenu();
+            }
+        });
 
-        tv_title = (TextView) findViewById(R.id.tv_title);
-        tv_source_date = (TextView) findViewById(R.id.tv_source_date);
+        postItem = (PostItem) getIntent().getSerializableExtra("postItem");
         wv_content = (WebView) findViewById(R.id.wv_content);
 
         getData();
         android.os.Looper looper = android.os.Looper.myLooper();
         msgHandler = new MessageHandler(looper);
+    }
+
+    public void popupmenu() {
+        popupMenu.show();
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     class MessageHandler extends android.os.Handler {
@@ -69,9 +147,6 @@ public class DetailActivity extends Activity {
         public void handleMessage(android.os.Message msg) {
             switch (msg.arg1) {
                 case msg_get_post_m_list:
-                    tv_title.setText(postItem.getTitle());
-                    Date _date = new Date();
-                    tv_source_date.setText(postItem.getOrigin() + "  " + postItem.getTime());
                     String jsonStr = (String) msg.obj;
                     try {
                         PostData result = new Gson().fromJson(jsonStr, PostData.class);
