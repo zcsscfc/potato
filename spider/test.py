9ed98c3e1f357388fd8f422c1bda6c7fe75623ca
lance <lance_li@outlook.com>
2016-05-27 16:20:30 +0800

    测试 spider

--- spider/test.py
+++ spider/test.py
@@ -5,6 +5,12 @@
 # http://lxml.de/api/lxml.cssselect-pysrc.html
 
 
+# 参考，天猫，描述图片
+
+# https://desc.alicdn.com/i2/130/710/13371072438/TB18hcHKXXXXXbzXVXX8qtpFXlX.desc%7Cvar%5Edesc%3Bsign%5E3c583fe1ae72146a31516233853bc4f1%3Blang%5Egbk%3Bt%5E1461227321
+
+
+
 
 
 
