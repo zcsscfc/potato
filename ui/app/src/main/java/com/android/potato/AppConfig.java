d003d387d0fc66f47d2850786c2f3d622bf0326b
lance <lance_li@outlook.com>
2016-05-28 10:07:28 +0800

    AppConfig新域名

--- ui/app/src/main/java/com/android/potato/AppConfig.java
+++ ui/app/src/main/java/com/android/potato/AppConfig.java
@@ -1,5 +1,5 @@
 package com.android.potato;
 
 public class AppConfig {
-    public static final String SERVER_URL = "http://ec2-52-196-163-180.ap-northeast-1.compute.amazonaws.com:8080/";
+    public static final String SERVER_URL = "http://ec2-52-193-201-108.ap-northeast-1.compute.amazonaws.com:8080/";
 }
