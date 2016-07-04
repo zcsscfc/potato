ef27130817e84c4c47ca31ad1920d874229f615d
lance <lance_li@outlook.com>
2016-07-04 17:42:17 +0800

    更新

--- spider/crawler/test_tmall.py
+++ spider/crawler/test_tmall.py
@@ -16,12 +16,9 @@ sys.path.append("../utility")
 sys.path.append("../service")
 from Common import Common
 
-sys.path.append("../orm")
-from PotatoDb import PotatoDb
-
 from bs4 import BeautifulSoup
 
-def DownLoad():
+def DownLoad(): 
 	origin_name = ''
 	url_seed = 'https://detail.tmall.com/item.htm?spm=a1z10.5-b.w4011-11611893624.94.s5fTwi&id=13371072438&rn=c135f614d00170bbf60fd1a112bd5174&abbucket=15'
 	url_pattern = 'no need pattern'
@@ -29,23 +26,23 @@ def DownLoad():
 	url_list = [url_seed]
 	while url_list:
 		url = url_list.pop()
-		
 		PROXY = '10.43.146.29:8080'
 		headers = {
 			'Accept': 'text/html, application/xhtml+xml, */*',
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-			'Accept-Encoding': 'gzip, deflate',
-			'DNT': '1',
+			'Content-type':'text/html;charset=utf-8',
 			'Connection': 'Keep-Alive',
 			'Cookie': 'cna=JWzNDxyeEUMCAXTimov8+66/; l=Alpa9qYBcVLQU4nde1rhm2Yw6g58nt5l; t=aaf34248d05832bc2b0224588ecb6c42; _tb_token_=VknDYrghLIXD; cookie2=47a5b832857e164c4ee6cea3378d6317; pnm_cku822=109UW5TcyMNYQwiAiwVQX1EeUR5RH5Cd0xiNGI%3D%7CUm5OcktwSXZPc0lyTHNLfyk%3D%7CU2xMHDJxPk82UjVOI1h2VngRd1snQSJEI107F2gFfgRlAmRKakQYeR9zFGoQPmg%2B%7CVGhXd1llXGdeYVhkXmVbZFxoX2JAdUx1S3VOc0x0QXtPek96Q207%7CVWldfS0TMws2Dy8TLw8hHz8DJnBacBsiW29BF0E%3D%7CVmhIGCAYOAUlGSceKgo0DjUAIBwiGSICOAM2FioULxQ0DjEEUgQ%3D%7CV25Tbk5zU2xMcEl1VWtTaUlwJg%3D%3D; cq=ccp%3D1'
 		}
-		#proxy_handler = urllib2.ProxyHandler({"https":PROXY, "http":PROXY})
-		#opener = urllib2.build_opener(proxy_handler, urllib2.HTTPHandler)
-		#urllib2.install_opener(opener)
-		html = urllib2.urlopen(url).read()
+		proxy_handler = urllib2.ProxyHandler({"https":PROXY, "http":PROXY})
+		opener = urllib2.build_opener(proxy_handler, urllib2.HTTPHandler)
+		urllib2.install_opener(opener)
+		request = urllib2.Request(url,'',headers)
+		html = urllib2.urlopen(request).read()
 		tree = lxml.html.fromstring(html)
+		print html
+		return
 		
-		#fixedHtml = lxml.html.tostring(fixedHtml, pretty_print=True,encoding="utf-8")
 		tree = tree.cssselect('ul#J_UlThumb')[0]
 		image_list = tree.cssselect('img')
 		
@@ -70,6 +67,7 @@ def GetPageUrlList():
 
 def GetPageItemUrlList(url):
 	print url
+	PROXY = '10.43.146.29:8080'
 	headers = {
 		'Accept': 'text/html, application/xhtml+xml, */*',
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
@@ -78,6 +76,9 @@ def GetPageItemUrlList(url):
 		'Connection': 'Keep-Alive',
 		'Cookie': 'cna=JWzNDxyeEUMCAXTimov8+66/; l=Alpa9qYBcVLQU4nde1rhm2Yw6g58nt5l; t=aaf34248d05832bc2b0224588ecb6c42; _tb_token_=VknDYrghLIXD; cookie2=47a5b832857e164c4ee6cea3378d6317; pnm_cku822=109UW5TcyMNYQwiAiwVQX1EeUR5RH5Cd0xiNGI%3D%7CUm5OcktwSXZPc0lyTHNLfyk%3D%7CU2xMHDJxPk82UjVOI1h2VngRd1snQSJEI107F2gFfgRlAmRKakQYeR9zFGoQPmg%2B%7CVGhXd1llXGdeYVhkXmVbZFxoX2JAdUx1S3VOc0x0QXtPek96Q207%7CVWldfS0TMws2Dy8TLw8hHz8DJnBacBsiW29BF0E%3D%7CVmhIGCAYOAUlGSceKgo0DjUAIBwiGSICOAM2FioULxQ0DjEEUgQ%3D%7CV25Tbk5zU2xMcEl1VWtTaUlwJg%3D%3D; cq=ccp%3D1'
 	}
+	proxy_handler = urllib2.ProxyHandler({"https":PROXY, "http":PROXY})
+	opener = urllib2.build_opener(proxy_handler, urllib2.HTTPHandler)
+	urllib2.install_opener(opener)
 	request = urllib2.Request(url,'',headers)
 	html = urllib2.urlopen(request).read()
 	tree = lxml.html.fromstring(html)
@@ -91,9 +92,9 @@ def GetPageItemUrlList(url):
 		print href
 		print name
 	
-GetPageUrlList()
+#GetPageUrlList()
 			
-#DownLoad()
+DownLoad()
 
 
 
